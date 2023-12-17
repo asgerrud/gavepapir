8e394f871e660851b184f3648ba3b926bbae53e6
Asger Rud Hansen <36601572+asgerrud@users.noreply.github.com>
2023-12-17 12:22:32 +0100

    Rewording

--- components/MainCard.tsx
+++ components/MainCard.tsx
@@ -11,7 +11,7 @@ const MainCard = () => {
   return (
     <div className="container max-w-[800px]">
       <div className="pt-12 pb-16 px-16 h-full bg-white shadow-md rounded-lg">
-        <h1 className="mb-8 text-center text-3xl font-extrabold">Gift Wrapping Paper Calculator</h1>
+        <h1 className="mb-8 text-center text-3xl font-extrabold">Gift Wrap Calculator</h1>
         <Form onSubmit={(dimensions) => setDimensions(dimensions)} />
         <div className="flex flex-col mt-10">
           <h2 className="font-bold text-xl mb-3">You will need</h2>
